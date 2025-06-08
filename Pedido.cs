48d5516133f93fc35fcfa1f06b4c81ec64252e32
GustavoHoriquiri <gustavo.horiquiri@icloud.com>
2025-06-08 20:13:26 -0300

    Update Pedido

--- Pedido.cs
+++ Pedido.cs
@@ -8,5 +8,28 @@ namespace Gestao_Pedidos_Loja_virtual
 {
     public class Pedido
     {
+        public int Id { get; }
+        public Cliente Cliente { get; }
+        public List<ItemPedido> Itens { get; }
+        public DateTime Data { get; }
+        public IDescontoStrategy DescontoStrategy { get; }
+
+        public Pedido(int id, Cliente cliente, List<ItemPedido> itens, IDescontoStrategy desconto)
+        {
+            Id = id;
+            Cliente = cliente;
+            Itens = itens;
+            Data = DateTime.Now;
+            DescontoStrategy = desconto;
+        }
+
+        public double ValorTotal()
+        {
+            double total = 0;
+            foreach (var item in Itens)
+                total += item.ValorTotal();
+
+            return DescontoStrategy.AplicarDesconto(Itens, total);
+        }
     }
 }
