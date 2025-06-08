0851ba1f41de572d5cae9dbd4a2eccde494c8d9a
GustavoHoriquiri <gustavo.horiquiri@icloud.com>
2025-06-08 20:20:59 -0300

    Create Pedido_P

---
+++ Pedido_P.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestao_Pedidos_Loja_virtual
+{
+    public class Pedido_P
+    {
+	}
+}
