53cf36a983c7e975cefc34cfb79c2b0ba92094d8
GustavoHoriquiri <gustavo.horiquiri@icloud.com>
2025-06-09 08:15:55 -0300

    Update Pedido_P

--- Pedido_P.cs
+++ Pedido_P.cs
@@ -8,5 +8,9 @@ namespace Gestao_Pedidos_Loja_virtual
 {
     public class Pedido_P
     {
-	}
+        public Pedido CriarPedido(int id, Cliente cliente, List<ItemPedido> itens, IDescontoStrategy desconto)
+        {
+            return new Pedido(id, cliente, itens, desconto);
+        }
+    }
 }
