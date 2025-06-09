adc2099a5547a60336a15737c9b5b53e2f775507
GustavoHoriquiri <gustavo.horiquiri@icloud.com>
2025-06-09 08:34:46 -0300

    Update PedidoService

--- PedidoService.cs
+++ PedidoService.cs
@@ -8,5 +8,17 @@ namespace Gestao_Pedidos_Loja_virtual
 {
     public class PedidoService
     {
+        private readonly ILogger _logger:
+
+        public PedidoService(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void RegistrarPedido(Pedido pedido, Pedido_P repo)
+        {
+            repo.Adicionar(pedido);
+            _logger.Log($"Pedido {pedido.id} registrado para {pedido.Cliente.Nome}");
+        }
 	}
 }
