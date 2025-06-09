486b0ea5750ff58e9e99219f1fe98420c05ab1a9
GustavoHoriquiri <gustavo.horiquiri@icloud.com>
2025-06-09 08:28:36 -0300

    Update ConsoleLogger

--- ConsoleLogger.cs
+++ ConsoleLogger.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Gestao_Pedidos_Loja_virtual
 {
-	public class ConsoleLogger
+	public class ConsoleLogger : ILogger
 	{
+		public void Log(string mensagem)
+		{
+			Console.WriteLine($"[LOG] {mensagem}");
+		}
 	}
 }
